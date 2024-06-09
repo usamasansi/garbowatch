fb8069cfa09d72c9f1caf33aa402495c3cb9139d
Raed Bin UMair <raedumair01@hmail.com>
2024-06-09 14:41:49 +0500

    classification

--- fypbackend/routes/login.js
+++ fypbackend/routes/login.js
@@ -7,12 +7,14 @@ router.post('/', async (req, res) => {
 
   try {
     const user = await User.findOne({ username, password });
+    console.log(user)
     
     if (!user) {
       return res.status(401).json({ error: 'Invalid username or password' });
+      
     }
 
-    return res.status(200).json({ message: 'Login successful' });
+    return res.status(200).json({ message: 'Login successful', data:user });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: 'Server error' });
